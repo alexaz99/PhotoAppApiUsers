17ade735cef806e42454ab1aee67af3f4e4cf760
Alex <azakotyansky@gmail.com>
2020-09-19 07:27:10 -0400

    Start implementing user login functionality

---
+++ src/main/java/com/alex/photoapp/users/ui/model/LoginUserRequestModel.java
@@ -0,0 +1,12 @@
+package com.alex.photoapp.users.ui.model;
+
+/**
+ * The class fields match the login request
+ * fields that a user send in json document over http request.
+ */
+public class LoginUserRequestModel {
+
+    private String email;
+    private String password;
+
+}
